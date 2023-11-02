acbcbd0b21a54d3eadeea9b674ef30a465aa14b1
heronziegel <tuo12440@temple.edu>
2023-11-02 14:34:31 -0400

    Converted function into a coroutine

--- app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
+++ app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
@@ -6,6 +6,12 @@ import android.os.Handler
 import android.os.Looper
 import android.widget.Button
 import android.widget.ImageView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,13 +30,25 @@ class MainActivity : AppCompatActivity() {
 
         cakeImageView = findViewById(R.id.imageView)
 
-        findViewById<Button>(R.id.revealButton).setOnClickListener{
-            Thread{
-                repeat(100) {
-                    handler.sendEmptyMessage(it)
-                    Thread.sleep(40)
-                }
-            }.start()
+//        findViewById<Button>(R.id.revealButton).setOnClickListener{
+//            Thread{
+//                repeat(100) {
+//                    handler.sendEmptyMessage(it)
+//                    Thread.sleep(40)
+//                }
+//            }.start()
+//        }
+
+        val scope = CoroutineScope(Job() + Dispatchers.Default)
+
+        scope.launch{countdownTimer()}
+    }
+
+    suspend fun countdownTimer(){
+        repeat(100){
+            withContext(Dispatchers.Main){handler.sendEmptyMessage(it)}
+            delay(40)
+
         }
     }
 }
